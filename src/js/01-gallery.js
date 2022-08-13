7f87e8c010a37b14b5bc0fb064f68b46a4475ac1
Olena-MB <81712181+Olena-MB@users.noreply.github.com>
2022-08-13 20:47:17 +0300

    push3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryImg = document.querySelector('.gallery');
+
+//Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+const imagesMarkup = galleryItems
+.map(({preview,original,description}) => 
+`<li class = "gallery__item">
+<a class = "gallery__item" href = "${original}">
+<img class = "gallery__image"
+  src = "${preview}"
+  alt = "${description}"/>
+</a></li>`).join("");
+
+galleryImg.insertAdjacentHTML('afterbegin', imagesMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
